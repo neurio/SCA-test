dfb7b8419ec3e73e54759537b879c781284b671c
KERRYUE <kerry.zhou@generac.com>
2021-02-22 21:29:34 -0800

    add python file

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -20,3 +20,66 @@ message = {(True, True): "FizzBuzz", (True, False): "Fizz", (False, True): "Buzz
 results = [message[(p,q)] or i for (i, p, q) in truth]
 for result in results:
     print(result)
+
+for i in range(1, 101):
+    print("Fizz"*(i % 3 == 0) + "Buzz"*(i % 5 == 0) or i, end=' ')
+
+for i in range(1, 101):
+    num = ''
+    if i % 3 == 0:
+        num += "Fizz"
+    if i % 5 == 0:
+        num += "Buzz"
+    if num == '':
+        num += str(i)
+    print(num)
+
+
+truth = [(i, not i % 3, not i % 5) for i in range(1, 101)]
+message = {(True, True): "FizzBuzz", (True, False): "Fizz",
+           (False, True): "Buzz", (False, False): ""}
+results = [message[(p, q)] or i for (i, p, q) in truth]
+for result in results:
+    print(result)
+
+for i in range(1, 101):
+    print("Fizz"*(i % 3 == 0) + "Buzz"*(i % 5 == 0) or i, end=' ')
+
+for i in range(1, 101):
+    num = ''
+    if i % 3 == 0:
+        num += "Fizz"
+    if i % 5 == 0:
+        num += "Buzz"
+    if num == '':
+        num += str(i)
+    print(num)
+
+
+truth = [(i, not i % 3, not i % 5) for i in range(1, 101)]
+message = {(True, True): "FizzBuzz", (True, False): "Fizz",
+           (False, True): "Buzz", (False, False): ""}
+results = [message[(p, q)] or i for (i, p, q) in truth]
+for result in results:
+    print(result)
+
+for i in range(1, 101):
+    print("Fizz"*(i % 3 == 0) + "Buzz"*(i % 5 == 0) or i, end=' ')
+
+for i in range(1, 101):
+    num = ''
+    if i % 3 == 0:
+        num += "Fizz"
+    if i % 5 == 0:
+        num += "Buzz"
+    if num == '':
+        num += str(i)
+    print(num)
+
+
+truth = [(i, not i % 3, not i % 5) for i in range(1, 101)]
+message = {(True, True): "FizzBuzz", (True, False): "Fizz",
+           (False, True): "Buzz", (False, False): ""}
+results = [message[(p, q)] or i for (i, p, q) in truth]
+for result in results:
+    print(result)
